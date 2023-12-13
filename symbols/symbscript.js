710fda8f1f987e57852577dc2ca8fdf1a30ea50f
chintooflutter <46987072+chintooflutter@users.noreply.github.com>
2023-12-13 20:25:56 +0000

    new chritmas

--- symbols/symbscript.js
+++ symbols/symbscript.js
@@ -1,15 +1,8 @@
-function copyText(text) {
-  var tempInput = document.createElement("textarea");
-  tempInput.value = text;
-  document.body.appendChild(tempInput);
-  tempInput.select();
-  document.execCommand("copy");
-  document.body.removeChild(tempInput);
-
-  updatePreview(text);
-}
-
-function updatePreview(symbol) {
-  var symbolPreview = document.getElementById('symbolPreview');
-  symbolPreview.innerText = symbol;
+function copySymbol(symbol) {
+    var tempInput = document.createElement("textarea");
+    tempInput.value = symbol;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
 }
