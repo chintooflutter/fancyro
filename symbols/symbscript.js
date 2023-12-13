3f1aec7381420a6d271f096becc3b8c9892ad1d0
chintooflutter <46987072+chintooflutter@users.noreply.github.com>
2023-12-13 20:23:21 +0000

    charitmas symbol

--- symbols/symbscript.js
+++ symbols/symbscript.js
@@ -1,31 +1,15 @@
-document.getElementById('symbolTable').addEventListener('click', function(event) {
-    if (event.target.classList.contains('copyButton')) {
-        var symbolCell = event.target.closest('tr').querySelector('td:first-child');
-        copyText(symbolCell.innerText);
-    }
-});
-
-document.getElementById('symbolTable').addEventListener('mouseover', function(event) {
-    if (event.target.tagName === 'TD') {
-        var symbolPreview = document.getElementById('symbolPreview');
-        symbolPreview.innerText = event.target.innerText;
-        symbolPreview.classList.add('selected');
-    }
-});
-
-document.getElementById('symbolTable').addEventListener('mouseout', function() {
-    var symbolPreview = document.getElementById('symbolPreview');
-    symbolPreview.innerText = 'Select a symbol to preview and copy here';
-    symbolPreview.classList.remove('selected');
-});
-
 function copyText(text) {
-    var tempInput = document.createElement("textarea");
-    tempInput.value = text;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
+  var tempInput = document.createElement("textarea");
+  tempInput.value = text;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
 
-    alert('Symbol copied: ' + text);
+  updatePreview(text);
+}
+
+function updatePreview(symbol) {
+  var symbolPreview = document.getElementById('symbolPreview');
+  symbolPreview.innerText = symbol;
 }
