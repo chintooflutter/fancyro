6d95c6fe23dcede350a4d34ac4c34ba42e412351
chintooflutter <46987072+chintooflutter@users.noreply.github.com>
2023-12-14 22:35:38 +0000

    new change

--- source/script.js
+++ source/script.js
@@ -1,7 +1,7 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
   // Fetch the navigation bar content
-  fetch("/source/navbar.html")
+  fetch("source/navbar.html")
     .then(response => response.text())
     .then(data => {
       // Insert the navigation bar into the container
