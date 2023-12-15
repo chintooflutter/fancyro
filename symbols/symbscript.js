15f14b9f3881cee4059a6182c379cf33a4ddaf3b
chintooflutter <46987072+chintooflutter@users.noreply.github.com>
2023-12-15 19:32:30 +0000

    new

--- symbols/symbscript.js
+++ symbols/symbscript.js
@@ -1,8 +1,8 @@
-function copySymbol(symbol) {
-    var tempInput = document.createElement("textarea");
-    tempInput.value = symbol;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
+function copyToClipboard(symbol) {
+        var tempInput = document.createElement("textarea");
+        tempInput.value = symbol;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
 }
