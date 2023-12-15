a3b4327656ce43c29d0dd7df6ae98de8398fdb62
chintooflutter <46987072+chintooflutter@users.noreply.github.com>
2023-12-15 13:15:15 +0000

    new change

--- source/navscript.js
+++ source/navscript.js
@@ -1,9 +1,6 @@
 const nav = document.querySelector('.navbar')
 fetch('source/navbar.html')
-.then(res=>res.text())
-.then(data=>{
-    nav.innerHTML=data
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(data, 'text/html')
-    eval(doc.querySelector('script').textContent)
-})
+  .then(res => res.text())
+  .then(data => {
+    nav.innerHTML = data
+  })
