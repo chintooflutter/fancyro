bf8488ccaf1fa6efd0ddf64d148e7996d095a819
chintooflutter <46987072+chintooflutter@users.noreply.github.com>
2024-04-25 19:33:28 +0000

    update

--- components/footer.js
+++ components/footer.js
@@ -4,10 +4,9 @@ class FooterComponent extends HTMLElement {
 
     this.attachShadow({ mode: "open" });
 
-    const template = document.createElement("template");
-    template.innerHTML = `
+    this.shadowRoot.innerHTML = `
       <style>
-        .footer-links {
+.footer-links {
           display: flex;
           justify-content: space-around;
           padding: 1rem;
@@ -28,20 +27,17 @@ class FooterComponent extends HTMLElement {
             align-items: center;
           }
         }
+
+
       </style>
 
       <footer class="bg-body-tertiary">
         <div class="footer-links">
           <a href="https://fancyletters.org/a-in-different-fonts.html">A in Different Fonts</a>
-
           <a href="https://fancyletters.org/a-in-cursive.html">How to Write A in Cursive</a>
-
           <a href="https://fancyletters.org/about.html">About</a>
-
           <a href="https://fancyletters.org/contact.html">Contact</a>
-
-      <a href="https://fancyletters.org/privacy.html">Privacy</a>		  
-
+          <a href="https://fancyletters.org/privacy.html">Privacy</a>
         </div>
 
         <!-- Copyright -->
@@ -50,14 +46,8 @@ class FooterComponent extends HTMLElement {
           <a class="text-body" href="https://fancyletters.org/sitemap.xml">FancyLetters.org</a>
         </div>
         <!-- Copyright -->
-
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
       </footer>
     `;
-
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
 
--- components/header.js
+++ components/header.js
@@ -4,11 +4,9 @@ class HeaderComponent extends HTMLElement {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
       <header>
-
-
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
           <a class="navbar-brand" href="https://fancyletters.org/">Home</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <button class="navbar-toggler" aria-label="Toggle navigation" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="collapsibleNavbar">
@@ -23,37 +21,15 @@ class HeaderComponent extends HTMLElement {
                 <a class="nav-link" href="https://fancyletters.org/zalgo-font-generator.html">Zalgo Font</a>
               </li>
               <li class="nav-item">
-        <a class="nav-link" href="https://fancyletters.org/fancy-font-generator.html">Fancy Font</a>
-        </li>                            
-        <li class="nav-item">
-        <a class="nav-link" href="https://fancyletters.org/symbols/christmas.html">Christmas Symbols</a>
-        </li>                            
+                <a class="nav-link" href="https://fancyletters.org/fancy-font-generator.html">Fancy Font</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://fancyletters.org/symbols/christmas.html">Christmas Symbols</a>
+              </li>
             </ul>
           </div>
         </nav>
-
-
-
       </header>
-
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  </link>
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  </link>
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
-
-
     `;
 
     const navbarToggler = this.shadowRoot.querySelector('.navbar-toggler');
